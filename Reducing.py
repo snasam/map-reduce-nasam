1c09ee3c44bcd351b5afbf5915c570825565076e
snasam <77593263+snasam@users.noreply.github.com>
2022-01-28 13:54:29 -0600

    Commit

--- Reducing.py
+++ Reducing.py
@@ -1,14 +1,27 @@
+s = open("s.txt","r")
+r = open("r.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+
 for line in s:
- data = line.strip().split('\t')
- store, amount = data
+  data = line.strip().split('\t')
+  store, amount = data
+  if store != thisKey:
+    if thisKey:
 
- if store != thisKey:
-   if thisKey:
-     # output the last key value pair result
-     r.write(thisKey + '\t' + str(thisValue)+'\n')
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
 
-   # start over when changing keys
-   thisKey = store
-   thisValue = 0.0
- # apply the aggregation function
- thisValue += float(amount)
+    # start over when changing keys
+    thisKey = store
+    thisValue = 0.0
+
+  # apply the aggregation function
+  thisValue += float(amount)
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
